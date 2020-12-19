eff768bbc5abba455a5a393d632088ef34eb7518
Aman Verma <amanvermaa29@gmail.com>
2020-12-19 18:02:32 +0530

    clean repo

--- Hiring/src/main/java/com/finahub/service/BankService.java
+++
@@ -1,8 +0,0 @@
-package com.finahub.service;
-
-import com.finahub.pojo.Bank;
-
-public interface BankService {
-	public Bank saveBankDetails(Bank data);
-	public Object getBankInfo();
-}
--- client/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import Bank from './Bank'
-function App() {
-  return (
-   <Bank/>
-  );
-}
-
-export default App;
